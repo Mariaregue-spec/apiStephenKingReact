633ed5bce209623816a61f0c2bcd3ce08ccdfc1b
Mariaregue-spec <mariareguecurso@gmail.com>
2026-02-09 22:08:55 +0100

    Fix:fix errors in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/Home";
-import Author from "./pages/Author";
-import Books from "./pages/Books";
-import Footer from "./components/footer/Footer";
-import bg from "./assets/images/background/backgroundpage1.png";
+import Navbar from "./components/navbar/navbar";
+import Home from "./pages/home";
+import Author from "./pages/author";
+import Books from "./pages/books";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${bg})` }}
+      style={{ backgroundImage: "url('/src/assets/images/background/backgroundpage1.png')" }}
     >
+      {/* Overlay oscuro para resaltar el contenido */}
       <div className="min-h-screen bg-black bg-opacity-70">
         <Navbar />
         <Routes>
@@ -23,8 +23,8 @@ function App() {
       </div>
       <Footer />
     </div>
+
   );
 }
 
 export default App;
-
